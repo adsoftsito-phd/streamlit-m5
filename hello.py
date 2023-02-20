6171b6d1e419366ecf48e6422596d56e92b1ad79
adsoftsito-phd <120610355+adsoftsito-phd@users.noreply.github.com>
2023-02-20 14:29:42 +0000

    agrego programa search-names

---
+++ hello.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+st.title('My first app - Adolfo Centeno Tellez')
+st.header('Seccion de Analisis')
+st.header('Seccion de Graficos')
+st.write('App para realizar prediccion de datos')
